35821e86497d3e3977e7ed78158021ce563dab04
moguri846 <dkffkekahsl1@naver.com>
2021-02-10 13:43:37 +0900

    index key add

--- src/components/utils/ImageSlider.js
+++ src/components/utils/ImageSlider.js
@@ -6,13 +6,18 @@ function ImageSlider(props) {
         <div>
             <Carousel autoplay>
                 {props.images.map((image, index) => (
-                    <Row justify="center" align="middle">
-                        <Col span={24} 
-                        style={{ margin: "0 auto", width: "95%", overflow: "hidden", display: "flex", justifyContent: "center"}}>
-                            <img
-                                style={{ margin: "0 auto", maxHeight: '150px' }}
-                                src={image} alt={index}
-                            />
+                    <Row justify="center" align="middle" key={index}>
+                        <Col
+                            span={24}
+                            style={{
+                                margin: '0 auto',
+                                width: '95%',
+                                overflow: 'hidden',
+                                display: 'flex',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <img style={{ margin: '0 auto', maxHeight: '150px' }} src={image} alt={index} />
                         </Col>
                     </Row>
                 ))}
