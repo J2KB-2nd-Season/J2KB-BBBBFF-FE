5caa8885455b6f819a699575bc30a1403baca320
moguri846 <dkffkekahsl1@naver.com>
2021-02-10 13:41:03 +0900

    reducer add

--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, JOIN_USER } from '../_actions/type';
+import { LOGIN_USER, JOIN_USER, LOGOUT_USER, AUTH_USER } from '../_actions/type';
 
 export default function (state = [], action) {
     switch (action.type) {
@@ -6,5 +6,11 @@ export default function (state = [], action) {
             return { ...state, loginSuccess: action.payload };
         case JOIN_USER:
             return { ...state, registerSuccess: action.payload };
+        case LOGOUT_USER:
+            return { ...state };
+        case AUTH_USER:
+            return { ...state, userData: action.payload };
+        default:
+            return { ...state };
     }
 }
